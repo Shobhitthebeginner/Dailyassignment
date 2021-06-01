a356e0870c0efe4ad474dbd99a32e9f0a6a488d5
Shobhitthebegineer <shobhit.18bcs1107@abes.ac.in>
2021-06-01 21:15:08 +0530

    day2

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .import views
+from django.contrib.auth.views import
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.indexView,name="home"),
+    path('dashboard/',views.dashboardViews,name="dashboard"),
+    path('login/',views.loginViews,name="login"),
+    path('register/',views.registerViews,name="register"),
+    path('logout/',views.logoutViews,name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-def home(request):
-    return render(request,"home.html")
+def indexView(request):
+    return render(request,'index.html')
+def dashboardViews(request):
+    return render(request,"dashboard.html")
+def loginViews(request):
+    return render(request,"login.html")
+def registerViews(request):
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=UserCreationForm()
+    return render(request,"register.html",{'form':form})
+def logoutViews(request):
+    return render(request,"logout.html")
